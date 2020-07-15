d7d9f8091e8de8f65838531fe4a7986366a147e0
tyneprincesa <j.nakitto@powem.net>
2020-07-15 12:47:36 +0300

    Transfer logic from /signup route to helper file

--- api/controllers/validationHelpers.js
+++ api/controllers/validationHelpers.js
@@ -1,5 +1,6 @@
 const pool = require('../models/dbconnect')
 
+
 const checkCondition = async(res, option, value)=>{
     try {
         const condition = `SELECT * FROM users WHERE ${option} = $1`
@@ -19,14 +20,42 @@ const checkCondition = async(res, option, value)=>{
 
 const addUser = async(res, username, name, email,password, age, gender, telephone, about_me, role)=>{
     try {
+        const regex = /\S+@\S+\.\S/
+        const isEmail = await checkCondition(res, 'email', email)
+        const isUsername = await checkCondition(res, 'username', username)
         const addNewUser = `INSERT INTO users (username, name, email, password, age, gender, telephone,  about_me, role, created_on, last_login)
                         VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, NOW(), NOW())`
         const addNewUserValues = [username, name, email, password, age, gender, telephone, about_me, role]
-
-        result = await pool.query(addNewUser, addNewUserValues)
-
-        return {
-            result: result.rows
+        
+        if (!email || !password){
+            return res.status(400).json({
+                message: 'Email and Password is required'
+            })
+        }else if (age &&  typeof age !==  'number'){
+            return res.status(400).json({
+                message: 'Age should be a number'
+            })
+        }else  if (email && isEmail.data.length){
+            return res.status(400).json({
+                message: 'Email already exists'
+            })
+        } else if(username && isUsername.data.length){
+            return res.status(400).json({
+                message: 'Username already exists'
+            })
+        }else if (regex.test(email) === false){
+            return res.status(400).json({
+                message: 'Please enter a valid email'
+            })
+        }else if (password.length < 6){
+            return res.status(400).json({
+                message: 'Password should contain 6 or more characaters'
+            })
+        }else {
+            await pool.query(addNewUser, addNewUserValues)
+            return res.status(201).json({
+                message: 'User created successfuly'
+            })
         }
     } catch (error) {
         return res.status(400).json({
--- api/routes/users.js
+++ api/routes/users.js
@@ -24,42 +24,11 @@ router.get('/', (req, res, next)=>{
 
 router.post('/signup', async (req, res, next)=>{
     try{
-        const regex = /\S+@\S+\.\S/
         const {username, name, email, password, age, gender, telephone, about_me, role} = req.body
-        const isEmail = await validation.checkCondition(res, 'email', email)
-        const isUsername = await validation.checkCondition(res, 'username', username)
         const hashedPassword = await bcrypt.hash(password, 8)
             
-        if (!email || !password){
-            return res.status(400).json({
-                message: 'Email and Password is required'
-            })
-        }else if (age &&  typeof age !==  'number'){
-            return res.status(400).json({
-                message: 'Age should be a number'
-            })
-        }else  if (email && isEmail.data.length){
-            return res.status(400).json({
-                message: 'Email already exists'
-            })
-        } else if(username && isUsername.data.length){
-            return res.status(400).json({
-                message: 'Username already exists'
-            })
-        }else if (regex.test(email) === false){
-            return res.status(400).json({
-                message: 'Please enter a valid email'
-            })
-        }else if (password.length < 6){
-            return res.status(400).json({
-                message: 'Password should contain 6 or more characaters'
-            })
-        }else {
-            validation.addUser(res, username, name, email, hashedPassword, age, gender, telephone, about_me, role)
-            return res.status(201).json({
-                    message: 'User created successfuly'
-                })
-        }
+        validation.addUser(res, username, name, email, hashedPassword, age, gender, telephone, about_me, role)
+
     } catch(err){
         return res.status(400).json({
             message: err.message
