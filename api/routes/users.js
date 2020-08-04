d75db18cb4378bbba85a9604234e0bd99bcc5891
tyneprincesa <j.nakitto@powem.net>
2020-08-04 23:38:53 +0300

    Verify login token / that user is logged in

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const user = require('../controllers/validationHelpers')
+const checkAuthorization = require('../middleware/checkAuthorization')
 
 const {addUser, tokenConfirmation, getAllUsers, login} = user
 
-router.get('/', getAllUsers)
+router.get('/', checkAuthorization, getAllUsers)
 router.post('/signup', addUser)
 router.get('/confirmation/:token', tokenConfirmation)
 router.post('/login', login)
