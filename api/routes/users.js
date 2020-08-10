e526472520b6d67486197267a036120e1d14b037
tyneprincesa <j.nakitto@powem.net>
2020-08-11 00:03:35 +0300

    Rename checkAuthorization to checkLogin

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const user = require('../controllers/validationHelpers')
-const checkAuthorization = require('../middleware/checkAuthorization')
+const checkLogin = require('../middleware/checkLogin')
 const permissions = require('../middleware/permissions')
 
-const {addUser, tokenConfirmation, getAllUsers, login} = user
-
-router.get('/', checkAuthorization, permissions('member'), getAllUsers)
+router.get('/', checkLogin, userRole('admin'), getAllUsers)
 router.post('/signup', addUser)
 router.get('/confirmation/:token', tokenConfirmation)
 router.post('/login', login)
