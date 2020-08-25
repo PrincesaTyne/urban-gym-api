647d197e12d79fc6dba267b1dad045b66c48532f
tyneprincesa <j.nakitto@powem.net>
2020-08-26 00:48:30 +0300

    Rebase and Cleanup code

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const user = require('../controllers/users')
 
-<<<<<<< HEAD
 const {addUser, emailConfirmation, getAllUsers, login} = user
 
 router.get('/', getAllUsers)
@@ -10,13 +9,5 @@ router.post('/signup', addUser)
 router.get('/confirmation/:token', emailConfirmation)
 router.post('/login', login)
 
-=======
-const {addUser, tokenConfirmation, getAllUsers, login} = user
-
-router.get('/', getAllUsers)
-router.post('/signup', addUser)
-router.get('/confirmation/:token', tokenConfirmation)
-router.post('/login', login)
->>>>>>> 82deb96331f0ab7fef0290d718ff23c4ccd604b9
 
 module.exports = router
